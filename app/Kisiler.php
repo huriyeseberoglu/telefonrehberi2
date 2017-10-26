ce84817e6af62f8de44934e1d5aadaee421c0a65
huriyeseberoglu <huriye_seberoglu1907@hotmail.com>
2017-10-26 14:10:32 +0300

    migrations 2

--- app/Kisiler.php
+++ app/Kisiler.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class kisi extends Model
+class Kisiler extends Model
 {
     protected $table = "kisiler";
     protected $fillable = ['adi','soyadi','telefon','diger_telefon','adres'];
